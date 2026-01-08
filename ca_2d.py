302aad9a376a0fb3e9b693f8915c23605c7c1dec
JosVaandering <143613604+JosVaandering@users.noreply.github.com>
2026-01-08 15:56:17 +0100

    Update ca_2d.py
    ";" weggehaald

--- ca_2d.py
+++ ca_2d.py
@@ -4,8 +4,8 @@ from grid import Grid
 class CA_2D(Grid):
 
     def __init__(self, cells: int, rules: str, boundry_conditions: BoundryConditions):
-        super().__init__(cells, dimensions=2, states=2, neighbours = 8, rules=rules, boundry_conditions=boundry_conditions);
+        super().__init__(cells, dimensions=2, states=2, neighbours = 8, rules=rules, boundry_conditions=boundry_conditions)
 
     def display(self) -> None:
         # visual functionality will be implemented here
-        raise NotImplementedError;
+        raise NotImplementedError
