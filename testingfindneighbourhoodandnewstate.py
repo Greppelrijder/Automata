6d10d1a07b8111a947ee2ba0f12f129963bd8b07
JosVaandering <143613604+JosVaandering@users.noreply.github.com>
2026-01-08 19:39:13 +0100

    kortere notatie voor celverandering

--- testingfindneighbourhoodandnewstate.py
+++ testingfindneighbourhoodandnewstate.py
@@ -33,28 +33,30 @@ def find_neigbours_update_cell(cellnumber:int, cells, rule:str):
         right_nb = cells[cellnumber + 1]
     elif cellnumber == len(cells) - 1:
         right_nb = cells[0]
-    pattern = f"{left_nb}{cell_state}{right_nb}"
-    match pattern:
-        case "111":
-            newcell_state = rule[0]
-        case "110":
-            newcell_state = rule[1]
-        case "101":
-            newcell_state = rule[2]
-        case "100":
-            newcell_state = rule[3]
-        case "011":
-            newcell_state = rule[4]
-        case "010":
-            newcell_state = rule[5]
-        case "001":
-            newcell_state = rule[6]
-        case "000":
-            newcell_state = rule[7]
+    #pattern = f"{left_nb}{cell_state}{right_nb}"
+    newcell_state = rule[7 - 4*left_nb - 2*cell_state - right_nb]
+    #match pattern:
+    #    case "111":
+    #        newcell_state = rule[0]
+    #    case "110":
+    #        newcell_state = rule[1]
+    #    case "101":
+    #        newcell_state = rule[2]
+    #    case "100":
+    #        newcell_state = rule[3]
+    #    case "011":
+    #        newcell_state = rule[4]
+    #    case "010":
+    #        newcell_state = rule[5]
+    #    case "001":
+    #        newcell_state = rule[6]
+    #    case "000":
+    #        newcell_state = rule[7]
     return newcell_state
 
 
 print(find_neigbours_update_cell(6, [1,1,1,0,0,1,0], "00011110")) #check
 print(cells_overview([1,1,1,0,0,1,0,0,1,1], "00011110")) #check
 print(run([1,1,1,0,0,1,0,0,1,1], 5, "00011110")) #check
+
 
