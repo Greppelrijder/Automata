9c2b98abdfe3d9d0347e6ac305f809b0b8771594
JosVaandering <143613604+JosVaandering@users.noreply.github.com>
2026-01-08 15:55:11 +0100

    Remove unnecessary semicolons and fix formatting
    ";" weggehaald

--- ca_1d.py
+++ ca_1d.py
@@ -4,8 +4,8 @@ from grid import Grid
 class CA_1D(Grid):
 
     def __init__(self, cells: int, rules: str, boundry_conditions: BoundryConditions):
-        super().__init__(cells, dimensions=1, states=2, neighbours = 2, rules=rules, boundry_conditions=boundry_conditions);
+        super().__init__(cells, dimensions=1, states=2, neighbours = 2, rules=rules, boundry_conditions=boundry_conditions)
 
     def display(self) -> None:
         # visual functionality will be implemented here
-        raise NotImplementedError;
+        raise NotImplementedError
