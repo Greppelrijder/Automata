4cf52f0e73c2b350a22a95a8177b70d9ec262821
Tom Stroo <tomstroo3@gmail.com>
2026-01-13 19:28:32 +0100

    changed ruleset validation flow.
    Problem: the function was returning a bool representing whether or not the ruleset is valid. There are different causes that can return 'False' (incorrect length; invalid characters), but for a descriptive error message we need to be able to distinguish between them.
    
    Solution: the validation function no longer returns a bool. Instead, it is called 'validate_ruleset' now and *it* is responsible for raising error messages. Now we will be able to distinguish between different error cases

--- grid.py
+++ grid.py
@@ -29,24 +29,17 @@ class Grid(ABC):
         self.ruleset: str = rules
         self.boundry_conditions: BoundryConditions = boundry_conditions
 
-        if not self.ruleset_is_valid():
-            raise InvalidRulesetError(f"Incorrect length of ruleset, length must be {self.amount_of_states * self.amount_of_neighbourhood_states()}.")
-        #WE MOETEN HIER EIGENLIJK NOG ONDERSCHEID MAKEN TUSSEN INCORRECTE LENGTE EN INCORRECTE STATE
-        #RETURN 0 IF INC. LENGTE --> ERROR HIERBOVEN (MISSCHIEN DE FUNCTIE AMOUNT_OF_NEIGHBOORHOOD_STATES NOG NUTTIGER MAKEN?)
-        #RETURN 1 IF INC. STATE --> ANDERE ERROR
-        #RETURN 2 IF CORRECT --> GEEN ERROR
+        self.validate_ruleset()
     
 
-    def ruleset_is_valid(self) -> bool:
+    def validate_ruleset(self) -> None:
 
         if len(self.ruleset) != self.amount_of_neighbourhood_states():
-            return False
+            raise InvalidRulesetError(f"Incorrect length of ruleset, length must be {self.amount_of_states * self.amount_of_neighbourhood_states()}.")
         
         possible_states = range(0, self.amount_of_states)
         if any(int(char) not in possible_states for char in self.ruleset):
-            return False
-        
-        return True
+            raise InvalidRulesetError(f"[another descriptive error message]")
 
     def amount_of_neighbourhood_states(self) -> int:
         return self.amount_of_states ** (self.amount_of_neighbours + 1)
