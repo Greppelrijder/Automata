6991d8469b9f3766374c58554e1b90288eb40816
JosVaandering <143613604+JosVaandering@users.noreply.github.com>
2026-01-14 10:45:31 +0100

    Enhance GUI with buttons for cellular automata control
    Added buttons for plotting and evolving cellular automata with input for running multiple times.

--- Visualisation.py
+++ Visualisation.py
@@ -10,16 +10,33 @@ canvas = tk.Canvas(root)
 canvas.place(x = 0, y = 0, width = 800, height = 400)
 
 #functions: We implement the functions that the buttons use right here
-def vooruit():
+#instead of running these functions we can maybe immediatly call the other ones
+def startup():
     pass
 
+def run_once():
+    pass
+
+def run_amount(run_amount):
+    pass
 
 #We implement the buttons placement overhere
-tekstvr = tk.StringVar()
-Avooruit = ttk.Entry(root, textvariable = tekstvr)
-Avooruit.place(x = 205, y = 440, width = 50, height = 40)
-fd = tk.Button(root, text = "Forward", command = lambda : vooruit())
-fd.place(x = 255, y = 460, width = 100, height = 20)
+#Maybe distinction between 1D and 2D?
+#test
 
+#startup
+startup_button = tk.Button(root, text = "Plot CA", command = lambda : startup())
+startup_button.place(x = 850, y = 230, width = 100, height = 50)
+
+#run_once
+run_once_button = tk.Button(root, text = "Evolve 1 time", command = lambda : run_once())
+run_once_button.place(x = 850, y = 280, width = 100, height = 50)
+
+#run_amount
+run_amount_input = tk.IntVar()
+amount_input = ttk.Entry(root, textvariable = run_amount_input)
+amount_input.place(x = 850, y = 400, width = 100, height = 50)
+run_amount_button = tk.Button(root, text = "Run n times", command = lambda : run_amount(run_amount_input))
+run_amount_button.place(x = 850, y = 350, width = 100, height = 50)
 
 root.mainloop()
