ba8f5a1c55fbd1c93d6a8316bdf36b8a639d401f
Tom Stroo <tomstroo3@gmail.com>
2026-01-16 17:34:57 +0100

    added input validation and warnings to 1d visualisation

--- screens/ca_1d_prep_screen.py
+++ screens/ca_1d_prep_screen.py
@@ -16,23 +16,25 @@ def run(root: tk.Misc, args) -> None:
     # creating widgets
     header = tk.Label(canvas, text="Creating 1D CA")
     go_back_button = tk.Button(canvas, text="back", command= lambda: execute(Screen.Main_menu, None))
-    size_entry = tk.Entry(canvas)
-    ruleset_entry = tk.Entry(canvas)
+    
+    size_slider = tk.Scale(canvas, from_ = 1, to = 21, orient="horizontal")
 
-    opt = tk.StringVar(value=BoundryConditions.Dirichlet0.name)
-    boundry_conditions_dropdown = tk.OptionMenu(canvas, opt, *BoundryConditions._member_names_)
+    ruleset = tk.StringVar()
+    ruleset_entry = tk.Entry(canvas, textvariable=ruleset)
+    invalid_ruleset_warning = tk.Label(canvas, fg="red", text="Ruleset must consist of 8 characters")
 
-    name_entry = tk.Entry(canvas)
-    create_button = tk.Button(canvas, text="Create", command = lambda: execute(Screen.CA_1D_simulation, (
-        int(size_entry.get()),
-        ruleset_entry.get(),
-        BoundryConditions[opt.get()],
-        name_entry.get()
-        )))
+    boundry_condition_choice = tk.StringVar(value=BoundryConditions.Dirichlet0.name)
+    boundry_conditions_dropdown = tk.OptionMenu(canvas, boundry_condition_choice, *BoundryConditions._member_names_)
+
+    name = tk.StringVar()
+    name_entry = tk.Entry(canvas, textvariable=name)
+    invalid_name_warning = tk.Label(canvas, fg="red", text="Name must be non-empty")
+    
+    create_button = tk.Button(canvas, text="Create", command = lambda: on_create())
 
     # widget presets
     try: # use presets that were passed through args
-        size_preset = str(int(args[0]))
+        size_preset = int(args[0])
         ruleset_preset = str(args[1])
         assert isinstance(boundry_conditions_preset := args[2], BoundryConditions)
         name_preset: str = str(args[3])
@@ -41,16 +43,51 @@ def run(root: tk.Misc, args) -> None:
         ruleset_preset = "00011110"
         boundry_conditions_preset = BoundryConditions.Dirichlet0
         name_preset = "New"  
-    size_entry.insert(0, size_preset)
-    ruleset_entry.insert(0, ruleset_preset)
-    name_entry.insert(0, name_preset)
-    opt.set(boundry_conditions_preset.name)
+    size_slider.set(size_preset)
+    ruleset.set(ruleset_preset)
+    name.set(name_preset)
+    boundry_condition_choice.set(boundry_conditions_preset.name)
 
     # placing widgets
     header.place(x=400, y=0)
     go_back_button.place(x=400, y=50)
-    size_entry.place(x=400, y=100)
+    size_slider.place(x=400, y=75)
     ruleset_entry.place(x=400, y=125)
     boundry_conditions_dropdown.place(x=400, y=150)
     name_entry.place(x=400, y=200)
     create_button.place(x=400, y=225)
+
+    # automatic warnings on invalid input
+    def validate_name() -> bool:
+        if len(name.get()) == 0:
+            invalid_name_warning.place(x=400, y=250)
+            return False
+        else:
+            invalid_name_warning.place_forget()
+            return True
+        
+    def validate_ruleset() -> bool:
+        ruleset_value = ruleset.get()
+        if len(ruleset_value) != 8:
+            invalid_ruleset_warning.config(text="Ruleset must consist of 8 characters")
+            invalid_ruleset_warning.place(x=400, y=250)
+            return False
+        elif not all(char in ["0", "1"] for char in ruleset_value):
+            invalid_ruleset_warning.config(text="Ruleset must consist of only 1's & 0's")
+            invalid_ruleset_warning.place(x=400, y=250)
+            return False
+        else:
+            invalid_ruleset_warning.place_forget()
+            return True
+
+    ruleset.trace_add("write", callback = lambda *args: validate_ruleset())
+    name.trace_add("write", callback= lambda *args: validate_name())
+
+    # check for valid input
+    def on_create() -> None:
+        if validate_name() and validate_ruleset():
+            size = int(size_slider.get())
+            ruleset_value = ruleset.get()
+            boundry_conditions = BoundryConditions[boundry_condition_choice.get()]
+            name_value = name.get()
+            execute(Screen.CA_1D_simulation, (size, ruleset_value, boundry_conditions, name_value))
