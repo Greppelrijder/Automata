0e97b076c2a2eb2c8e1e491109de36686a5ca8a8
Tom Stroo <tomstroo3@gmail.com>
2026-01-22 14:25:59 +0100

    formatting

--- screens/main_menu.py
+++ screens/main_menu.py
@@ -13,20 +13,19 @@ class MainMenu(Screen):
 
         self.header: tk.Label = tk.Label(self.frame, text="Main menu", font=custom_font, padx=30, pady=50, justify="center", background="#8D8A8A")
         self.go_back_button: tk.Button = tk.Button(self.frame, text="back", border=5,background="#2DE840", activebackground="#178122", 
-                                                     fg="#202020", activeforeground="#202020", font=custom_font, anchor="center", 
+                                                     fg="#202020", activeforeground="#202020", font=custom_font, justify="center", 
                                                      command= lambda: execute(ScreenList.StartingScreen, None))
         self.ca_1d_button: tk.Button = tk.Button(self.frame, text="1D", border=5,background="#2DE840", activebackground="#178122", 
-                                                     fg="#202020", activeforeground="#202020", font=custom_font, anchor="center",
+                                                     fg="#202020", activeforeground="#202020", font=custom_font, justify="center",
                                                      command = lambda: execute(ScreenList.CA1D_Preparation, None))
         self.ca_2d_button: tk.Button = tk.Button(self.frame, text="2D", border=5,background="#2DE840", activebackground="#178122", 
-                                                     fg="#202020", activeforeground="#202020", font=custom_font, anchor="center",
+                                                     fg="#202020", activeforeground="#202020", font=custom_font, justify="center",
                                                      command= lambda: execute(ScreenList.CA2D_Preparation, None))
 
 
     def run(self, args) -> None:
         self.frame.place(x = 0, y = 0)
-        self.header.place(x=350, y=10)
-        self.go_back_button.place(relx=0.01, rely=0.01)
-        self.ca_1d_button.place(x=300, y=200)
-        self.ca_2d_button.place(x=600, y=200)
-
+        self.header.place(relx=0.5, rely=0.15, anchor="center")
+        self.go_back_button.place(relx=0.02, rely=0.02, anchor="nw")
+        self.ca_1d_button.place(relx=1/3, rely=0.55, anchor="center")
+        self.ca_2d_button.place(relx=2/3, rely=0.55, anchor="center")
--- screens/starting_screen.py
+++ screens/starting_screen.py
@@ -19,39 +19,11 @@ class StartingScreen(Screen):
 
         self.header: tk.Label = tk.Label(self.frame, text="Cellular automata", font=custom_font, padx=30, pady=50, justify="center", background="#8D8A8A")
         self.main_menu_button = tk.Button(self.frame, border=5,background="#2DE840", activebackground="#178122", 
-                                                     text="Main menu",fg="#202020", activeforeground="#202020", font=custom_font, anchor="center", 
+                                                     text="Main menu",fg="#202020", activeforeground="#202020", font=custom_font, 
+                                                     justify="center",
                                                      command= lambda : execute(ScreenList.MainMenu, None))
 
     def run(self, args) -> None:
         self.frame.place(x = 0, y = 0)
-        self.header.place(x=300, y=20)
-        self.main_menu_button.place(x=400, y=200)
-
-# gif = "Gospers_glider_gun.gif"
-# img = Image.open(gif)
-
-# frames = img.n_frames  # number of frames
-
-# photoimage_objects = []
-# for i in range(frames):
-#     obj = tk.PhotoImage(file=gif, format=f"gif -index {i}")
-#     photoimage_objects.append(obj)
-
-
-# def animation(current_frame=0):
-#     global loop
-#     image = photoimage_objects[current_frame]
-
-#     gif_label.configure(image=image)
-#     current_frame = (current_frame + 1) % frames
-
-#     if current_frame == frames:
-#         current_frame = 0
-
-#     loop = root.after(50, lambda: animation(current_frame))
-
-# gif_label = tk.Label(root, image="")
-# gif_label.pack()
-
-# start = tk.Button(root, text="Start", command=lambda: animation(current_frame=0))
-# start.pack()
+        self.header.place(relx=0.5, rely=0.15, anchor="center")
+        self.main_menu_button.place(relx=0.5, rely=0.5, anchor="center")
